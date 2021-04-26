11734715e944a4cf1fde8fe0d110fadb9905f58c
Sascha Egerer <sascha-egerer@users.noreply.github.com>
2021-04-26 14:03:17 +0200

    Adjust composer dependencies to support TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.5.99',
         ],
         'conflicts' => [
         ],
